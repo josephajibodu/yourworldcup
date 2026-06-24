f953a1a01306af416c49da136f3fabef96c1ec04
Joseph Ajibodu <josephajibodu@gmail.com>
2026-06-24 20:44:27 +0100

    remove testing user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,10 +19,5 @@ class DatabaseSeeder extends Seeder
             PredictionMarketSeeder::class,
             WorldCupSeeder::class,
         ]);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
